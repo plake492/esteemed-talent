d50eaa8544cd28184e53fe6fb0ddcbdb4f2f100c
Patrick Lake <plake492@gmail.com>
2020-12-17 16:59:37 -0500

    Work on talent home

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :style="'background-color: ' + color" class="wrapper">
+    <div class="container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+  props: {
+    color: {
+      type: String,
+      default: '#fff'
+    }
+  }
+}
+</script>
+
+<style>
+.wrapper {
+  border-bottom: 1px black solid;
+}
+</style>
