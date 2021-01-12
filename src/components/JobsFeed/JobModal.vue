4151594e9c290233a2362eb9c769fc21671c7190
Patrick Lake <plake492@gmail.com>
2021-01-12 17:04:34 -0500

    Add applicant form // File upload not currently working

--- src/components/JobsFeed/JobModal.vue
+++ src/components/JobsFeed/JobModal.vue
@@ -11,12 +11,38 @@
       </div>
       <h5>Remote | Contract</h5>
     </header>
-    <main class="p-5">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat aperiam
-      ex doloribus illo maiores, quasi porro, id, explicabo repellat deleniti
-      tempora perferendis culpa officia facere officiis placeat beatae ipsum
-      adipisci!
-    </main>
+    <form class="p-5">
+      <ul>
+        <li class="form-group">
+          <label for="first_name">FIRST NAME</label>
+          <input
+            v-model="applicant.firstName"
+            class="form-control"
+            type="text"
+          />
+        </li>
+        <li class="form-group">
+          <label for="last_name">LAST NAME</label>
+          <input
+            v-model="applicant.lastName"
+            class="form-control"
+            type="text"
+          />
+        </li>
+        <li class="form-group">
+          <label for="email">EMAIL</label>
+          <input v-model="applicant.email" class="form-control" type="email" />
+        </li>
+        <li class="form-group">
+          <label for="email">MOBILE PHONE</label>
+          <input v-model="applicant.phone" class="form-control" type="phone" />
+        </li>
+        <li class="form-group mt-5">
+          <label for="resume">UPLOAD RESUME</label>
+          <input type="file" class="file_upload w-100 p-4 text-center" />
+        </li>
+      </ul>
+    </form>
     <footer class="d-flex">
       <button @click="$emit('hideModal')" class="btn btn-secondary">
         <div>CANCEL</div>
@@ -37,12 +63,22 @@ export default {
       require: true
     }
   },
+  data() {
+    return {
+      applicant: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: ''
+      }
+    }
+  },
   methods: {
     hideModal() {
       this.$refs['my-modal'].hide()
     },
     submit() {
-      this.$emit('submitApplication', 'Bob')
+      this.$emit('submitApplication', this.applicant)
     }
   }
 }
@@ -58,6 +94,16 @@ button {
   width: 50%;
   margin: 0.5rem;
 }
+
+ul li::before {
+  content: '\2022';
+  color: red;
+  font-weight: bold;
+  display: inline-block;
+  width: 1em;
+  margin-left: -1em;
+}
+
 .escape {
   width: 20px;
   /* height: 20px; */
@@ -67,5 +113,8 @@ button {
 }
 .escape:hover {
   background-color: #474b57;
+}
+.file_upload {
+  border: 1px dashed black;
 }
 </style>
