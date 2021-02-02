3fe7565f1afb0b09488a03ae2c1315d0af23d3a7
Patrick Lake <plake492@gmail.com>
2021-02-01 19:28:55 -0500

    Add some css pizzazz

--- src/App.vue
+++ src/App.vue
@@ -277,6 +277,13 @@ input:focus {
   margin: 0.5em 0;
 }
 
+.hover_move {
+  transition: 0.3s;
+}
+.hover_move:hover {
+  transform: translateX(-5px);
+}
+
 @media (max-width: 991.98px) {
   .container {
     max-width: 100vw;
