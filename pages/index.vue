46de55b79c1db0183b3be5f916801b5fab4ef4a5
Patrick Lake <plake492@gmail.com>
2021-04-15 08:29:15 -0400

    rename _.vue to index.vue

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div style="margin-top: 15rem">
+      <p class="h2 text-center">Loading Jobs</p>
+      <nuxt-link class="mt-5 d-block text-center mx-auto" to="/jobs">
+        <div class="hover_move">
+          <BIconChevronLeft />
+          <span class="ml-2">{{ !err ? 'Back' : 'Browse Open Jobs' }}</span>
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { BIconChevronLeft } from 'bootstrap-vue'
+
+export default {
+  name: '404',
+  components: {
+    BIconChevronLeft
+  },
+  created() {
+    this.$router.push('/jobs')
+  }
+}
+</script>
+<!-- 
+<template>
+  <div class="container pb-5 mt-4">
+    <h2 class="font-weigth-bolder text-center">Open Jobs</h2>
+    <JobsFeedDisplay />
+  </div>
+</template>
+
+<script>
+import JobsFeedDisplay from '~/components/Jobs/JobsFeedDisplay'
+
+export default {
+  name: 'Jobs',
+  componenets: { JobsFeedDisplay },
+  head() {
+    return {
+      meta: [
+        {
+          vmid: 'description',
+          name: 'description',
+          content:
+            'Reach new heights in your career with Esteemed. Check out our latest job openings and opportunities.'
+        }
+      ]
+    }
+  }
+}
+</script> -->
