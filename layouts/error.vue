aafca0ff5e15dc756eed850a88b4ddd225a29845
Patrick Lake <plake492@gmail.com>
2021-09-10 15:52:28 -0400

    Add error layout for 404 page requests

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <main
+    class="container mt-5 mt-lg-0 d-flex align-items-center justify-content-center"
+  >
+    <div class="text-center">
+      <h3 class="mb-5">Error Loading Page</h3>
+      <nuxt-link class="mb-5" to="/jobs">
+        <div class="hover_move">
+          <BIconChevronLeft />
+          <span class="ml-2">{{ !err ? 'Back' : 'Browse Open Jobs' }}</span>
+        </div>
+      </nuxt-link>
+    </div>
+  </main>
+</template>
+
+<script>
+import { BIconChevronLeft } from 'bootstrap-vue'
+
+export default {
+  name: 'error',
+  components: {
+    BIconChevronLeft
+  }
+}
+</script>
+
+<style scoped>
+main {
+  min-height: 70vh;
+}
+</style>
